80b415d5065afe7d5adaed87966984e1ba11187a
kennedy bernard <makuanikennedy@gmail.com>
2022-02-27 19:18:32 +0300

    tried to find the problem but yet not seen

--- photo/urls.py
+++ photo/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.fashion,name = 'fashion'),
-    path(r'search/', views.search_results, name='search_results'),
-    path(r'image/(\d+)',views.image,name ='image'),
-    path(r'location/(\d+)',views.filter_by_location,name='location')
+    path('search/', views.search_results, name='search_results'),
+    path('image/(\d+)',views.image,name ='image'),
+    path('location/(\d+)',views.filter_by_location,name='location')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
