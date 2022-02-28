119be0f4e8a1b2722982b8d92a774dadc72ed3f3
kennedy bernard <makuanikennedy@gmail.com>
2022-02-28 10:16:18 +0300

    folders which were removed during fixing of the application

--- photo/models.py
+++ photo/models.py
@@ -8,6 +8,7 @@ import datetime as dt
 class Photographer(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
+    email = models.EmailField()
     
     def __str__(self):
         return self.first_name
