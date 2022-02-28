41c1e451a5d41d44d90413fa5da80e046e328758
kennedy bernard <makuanikennedy@gmail.com>
2022-02-28 10:26:18 +0300

    new changes

---
+++ photo/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import path,re_path
+from .. import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('',views.fashion,name = 'fashion'),
+    path('search/', views.search_results, name='search_results'),
+    re_path(r'image/(\d+)',views.image,name ='image'),
+    re_path(r'location/(\d+)',views.filter_by_location,name='location')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,37 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse, Http404# responsible for returning a response to a user
+import datetime as dt
+from .models import Image,Location,Photographer,Category
+
 
 # Create your views here.
+def fashion(request):
+    images = Image.objects.all()
+    return render(request, 'fashion.html',{'images':images})
+
+def search_results(request):
+    
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_photos = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photo/search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photo/search.html',{"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-photo/image.html", {"image":image})
+def filter_by_location(request,location_id):
+   """
+   Function that filters images by location
+   """
+   images = Image.filter_by_location(id=location_id )
+   return render (request, 'location.html', {"images":images})
